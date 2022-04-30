eb2f2772b7dca252184a40c089da6b548eb6724c
RAJ <rajkumarsah30@gmail.com>
2022-04-30 16:09:40 +0545

    Updated at Baishakh 17th

--- userinterface/views.py
+++ userinterface/views.py
@@ -6,4 +6,7 @@ from django.http import HttpResponse
 def index(request):
     return render(request, 'index.html')
 
+def login(request):
+    return render(request, 'login.html')
+
 #    return HttpResponse("Hello, world. You're at the  UI.")

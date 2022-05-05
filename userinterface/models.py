e3c9527fd96721fcaa56a8167f0a01f94c362395
RAJ <rajkumarsah30@gmail.com>
2022-05-06 00:05:55 +0545

    File taken as INPUT

--- userinterface/models.py
+++ userinterface/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class myuploadfile(models.Model):
+    fName = models.CharField(max_length=255)
+    myFile = models.FileField(upload_to="")
+
+    def __str__(self):
+        return self.fName
+    
+
--- userinterface/views.py
+++ userinterface/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from . models import myuploadfile
 # Create your views here.
 
 def index(request):
@@ -8,5 +9,14 @@ def index(request):
 
 def login(request):
     return render(request, 'login.html')
+
+def send_file(request):
+    if request.method == "POST":
+        name = request.POST.get("filename")
+        myfile = request.FILES.get("uploadfiles")
+        myuploadfile(fName=name, myFile=myfile).save()
+
+        return HttpResponse("Uploaded!!")
+
 
 #    return HttpResponse("Hello, world. You're at the  UI.")

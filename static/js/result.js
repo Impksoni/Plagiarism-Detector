861c3b0c41eb082e2b075d0294e07fd1d4b20589
Anees Banjara <aneesbanjara488@gmail.com>
2022-05-07 12:02:45 +0545

    submit directed to result result.js eventlistener eliminated

--- static/js/result.js
+++ static/js/result.js
@@ -1,14 +1,38 @@
-const result = document.querySelector('#result')
-result.addEventListener("click", (e) => {
-  e.preventDefault()
+const test = `[
+  {
+    "documentName": "A comparative study of ANN.",
+    "palgarizedPercent": 40
+  }, {
+    "documentName": "A comparative study of ANN3.",
+    "palgarizedPercent": 55
+  },  {
+    "documentName": "A comparative study of ANN4.",
+    "palgarizedPercent": 25
+  }, {
+    "documentName": "A comparative study of ANN6.",
+    "palgarizedPercent": 10
+  },  {
+    "documentName": "A comparative study of ANN7.",
+    "palgarizedPercent": 20
+  }
+]`
 
-  
+
+
+const result = document.querySelector('.uploadBtn')
+const object = JSON.parse(test)
+for(var i = 0; i < object.length; i++) {
+  if (object[i].palgarizedPercent < 40){
+    var concusion = 'pass'
+  } else {
+    concusion = 'fail'
+  }
   const row = document.createElement('tr')
   row.innerHTML = `
-  <td>random document</td>
-  <td>55%</td>
-  <td>fail</td>
+  <td>` + object[i].documentName +`</td>
+  <td>`+ object[i].palgarizedPercent + `</td>
+  <td>` + concusion + `</td>
   `
   const tablebody = document.querySelector('tbody')
   tablebody.appendChild(row)
-})
+}
